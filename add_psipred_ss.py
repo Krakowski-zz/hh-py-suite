8d3982bf4c33b69c06301129a5b242630248c1ac
krakos <krakowskikamil11@gmail.com>
2022-05-14 23:23:15 +0200

    added some functions

--- add_psipred_ss.py
+++ add_psipred_ss.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import shutil, sys
+def read_conf_file(pth):
+    config = {}
+    with open(pth, 'r') as config_file:
+        for line in config_file:
+            nm, val = line.split('::')
+            config[nm.strip()] = val.strip()
+    return config
+
+def parse_cmd_args():
+    args = {}
+    for x in range(len(sys.argv)):
+        if sys.argv[x].startswith('-'):
+            args[sys.argv[x][1:]] = sys.argv[x+1]
+        else:
+            continue
+
+shutil.copy2(src, dst)
