4f40adc37b22b8efeb7d765f9c6f338e1e2e9884
krakos <krakowskikamil11@gmail.com>
2022-06-03 00:42:21 +0200

    add -M parameter

--- add_psipred_ss.py
+++ add_psipred_ss.py
@@ -1,6 +1,6 @@
 import shutil, sys, os, time, subprocess
 
-def read_conf_file(pth="config.cfg"):
+def read_conf_file(pth=os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))+"/config.cfg"):
     config = {}
     with open(pth, 'r') as config_file:
         for line in config_file:
@@ -24,7 +24,7 @@ def parse_horiz(horiz_str):
     sequence = ""
     predicted_structure = ""
     confidence = ""
-    for part in result.split('\n\n'):
+    for part in horiz_str.split('\n\n'):
         part = part.strip()
         if not part or part.startswith("#"):
             continue
@@ -33,6 +33,31 @@ def parse_horiz(horiz_str):
         predicted_structure += pred[6:]
         confidence += conf[6:]
     return (sequence, predicted_structure, confidence)
+
+def parse_fasta(filein):
+    fin = open(filein, 'r')
+    previous_line = fin.__next__()
+    sequences = []
+    name = ""
+    sequence = ""
+    for line in fin:
+        if previous_line.strip()[0] == '>':
+            if name != "":
+                sequences.append((name, sequence))
+            name = previous_line.strip()[1:]
+            sequence = ""
+        else:
+            sequence+=previous_line.strip()
+        previous_line = line
+    else:
+        sequence+=previous_line.strip()
+        sequences.append((name, sequence))
+    return sequences
+
+def strip_alignment(filein, fileout):
+    with open(fileout, 'w') as fout:
+        for name, seq in parse_fasta(filein):
+            fout.write('>'+name+'\n'+seq.translate({45:None})+'\n')
 
 def print_error_cont(err):
     print (''.join(['# '+x for x in err.strip().split('\n')]))
@@ -56,6 +81,7 @@ os.makedirs(os.path.dirname(dirname), exist_ok=True)
 filepath = dirname+filename
 
 tmp["input_copy"] = filepath + "." + ext
+tmp["file_fastaseqs"] = filepath + ".fastaseqs"
 tmp["file_a3m"] = filepath + ".a3m"
 tmp["file_a3m_with_consensus"] = filepath + "_cons.a3m"
 tmp["file_consensus"] = filepath + ".cons"
@@ -70,7 +96,7 @@ shutil.copy2(param["input"], tmp["input_copy"])
 
 #convert alignment file
 if ext in available_formats:
-    convert_alignment(config["reformat"], tmp["input_copy"], tmp['file_a3m'], ext, "a3m", ["-M first"])
+    convert_alignment(config["reformat"], tmp["input_copy"], tmp['file_a3m'], ext, "a3m", ["-M "+str(param["M"])] if "M" in param else ["-M first"])
 
 #count consensus structure
 print('- '+str(time.strftime('%X %x'))+" building consensus sequence")
@@ -81,10 +107,11 @@ if err:
     print('- '+str(time.strftime('%X %x'))+" following problems occurred during consensus sequence building: ")
     print_error_cont(err.decode("utf-8"))
 
-
+#convert alignment to regular fasta sequences (not aligned)
+strip_alignment(tmp["input_copy"], tmp["file_fastaseqs"])
 #make blast database
 print('- '+str(time.strftime('%X %x'))+" creating BLAST database")
-cmd_mkblastdb = subprocess.Popen([config["makeblastdb"], "-in", tmp['file_a3m'], "-dbtype", "prot", "-out", filepath],
+cmd_mkblastdb = subprocess.Popen([config["makeblastdb"], "-in", tmp["file_fastaseqs"], "-dbtype", "prot", "-out", filepath],
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, err = cmd_mkblastdb.communicate()
 if err:
@@ -124,7 +151,7 @@ open(tmp["file_ss"], "wb").write(output)
 
 #generate .horiz file
 print('- '+str(time.strftime('%X %x'))+" generating .horiz file")
-cmd_psipass2 = subprocess.Popen([config["psipass2"], config["psipred_weights_p2"], "1", "1.0", "1.0", tmp["file_psipred_out"], tmp["file_ss"]], 
+cmd_psipass2 = subprocess.Popen([config["psipass2"], config["psipred_weights_p2"], "1", "1.0", "1.0", tmp["file_psipred_out"], tmp["file_ss"]],
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, err = cmd_psipass2.communicate()
 if not output:
@@ -134,12 +161,13 @@ if not output:
 result = output.decode("ASCII")
 sequence, predicted_structure, confidence = parse_horiz(result)
 
+#TODO: make sequence line length compatible with output format 
 with open(param["output"], 'w') as out_f:
     out_f.write(">Consensus\n")
     out_f.write(sequence+'\n')
     out_f.write(">ss_pred\n")
     out_f.write(predicted_structure+'\n')
-    out_f.write(">Confidence\n")
+    out_f.write(">ss_conf\n")
     out_f.write(confidence+'\n')
     out_f.write(open(tmp["file_a3m"]).read())
 
